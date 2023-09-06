cddc63da546be169521d928aed22ba28e0e1c129
Paul-1783 <paulschmitt144@gmail.com>
2023-09-06 09:33:11 +0200

    implemented getAccesToken

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -11,6 +11,7 @@ const redirect_uris = ["https://paul-1783.github.io/meet/"];
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
   CLIENT_SECRET,
+  CALENDAR_ID,
   redirect_uris[0]
 );
 
@@ -34,4 +35,40 @@ module.exports.getAuthURL = async () => {
       authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(response);
+    });
+  })
+    .then((results) => {
+      //Respond with OAuth token
+      return {
+        statuscode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 };

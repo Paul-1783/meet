a6cf7bd8d71bda1b6fc00a2de16c9f044bf7913c
Paul-1783 <paulschmitt144@gmail.com>
2023-10-23 16:59:03 +0200

    Adjusted css classnames

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,11 +3,14 @@ import { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   return (
-    <li>
+    <li className="event">
       <h3>{event ? event.summary : null}</h3>
       <p>{event ? event.location : null}</p>
       <p>{event ? event.created : null}</p>
-      <button onClick={() => setShowDetails(!showDetails)}>
+      <button
+        className="details-btn"
+        onClick={() => setShowDetails(!showDetails)}
+      >
         {showDetails ? "Hide Details" : "Show Details"}
       </button>
       {showDetails ? (

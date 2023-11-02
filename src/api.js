3b4b886d6569ea94be641b786fb7b66b3f7e260a
Paul-1783 <paulschmitt144@gmail.com>
2023-11-02 18:09:40 +0100

    Implemented offline storage of previously loaded events

--- src/api.js
+++ src/api.js
@@ -69,6 +69,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
   if (token) {
     removeQuery();
@@ -80,6 +86,7 @@ export const getEvents = async () => {
     const result = await response.json();
     if (result) {
       NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else {
       NProgress.done();
